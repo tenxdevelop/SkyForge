22645f57edfa90c5632e73258ecc5ec1599e6786
Hard_Code <zero-0_0@bk.ru>
2024-03-29 13:00:06 +0500

    Added BaseViewModel

---
+++ SkyForgeEditor/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace SkyForgeEditor.ViewModel.Base
+{
+    internal abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        protected virtual void PropertyChange([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T? field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (field?.Equals(value) ?? true)
+                return false;
+            
+            field = value;
+            PropertyChange(PropertyName);
+            return true;
+        }
+
+    }
+}
