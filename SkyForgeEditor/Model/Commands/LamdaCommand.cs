1e84f9b94b1c9d09420f3b48a466fabe5525ca3e
Hard_Code <zero-0_0@bk.ru>
2024-04-02 21:00:55 +0500

    added LamdaCommand

---
+++ SkyForgeEditor/Model/Commands/LamdaCommand.cs
@@ -0,0 +1,21 @@
+﻿using SkyForgeEditor.Model.Commands.Base;
+
+
+namespace SkyForgeEditor.Model.Commands
+{
+    public class LamdaCommand : BaseCommand
+    {
+        private Action<object> m_execute;
+        private Func<object, bool> m_canExecute;
+
+        public LamdaCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            m_execute = execute;
+            m_canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object? parameter) => m_canExecute(parameter);
+
+        public override void Execute(object? parameter) => m_execute(parameter);
+    }
+}
