03e10cb89997d384363182c98177f4d9549f095c
Hard_Code <zero-0_0@bk.ru>
2024-04-05 14:49:47 +0500

    addded basic template project

---
+++ SkyForgeEditor/Model/ProjectTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace SkyForgeEditor.Model
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+
+        [DataMember]
+        public string ProjectFile { get; set; }
+
+        [DataMember]
+        public List<string> Folders { get; set; }
+
+        public byte[] Icon { get; set; }
+        public byte[] Screenshot { get; set; }
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilePath { get; set; }
+
+    }
+}
--- SkyForgeEditor/ViewModel/ProjectBrowserWindowViewModel.cs
+++ SkyForgeEditor/ViewModel/ProjectBrowserWindowViewModel.cs
@@ -1,5 +1,9 @@
-﻿using SkyForgeEditor.Model.Commands;
+﻿using SkyForgeEditor.Extentions;
+using SkyForgeEditor.Model;
+using SkyForgeEditor.Model.Commands;
 using SkyForgeEditor.ViewModel.Base;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
 
@@ -10,8 +14,11 @@ namespace SkyForgeEditor.ViewModel
         private readonly Thickness POSITION_WINDOW_CREATE_PROJECT = new Thickness(-880, 0, 0, 0);
         private readonly Thickness POSITION_WINDOW_PROJECT = new Thickness(0, 0, 0, 0);
 
-        public Thickness PositionWidnowStack { get => m_positionWindowStack; set => Set(ref m_positionWindowStack, value); }
+        // TODO: get the path from the instalkation location 
+        private readonly string m_templatePath = @"..\..\..\SkyForgeEditor\Data\ProjectTemplates";
 
+        public ReadOnlyObservableCollection<ProjectTemplate>? ProjectTempaltes { get; private set; }
+        public Thickness PositionWidnowStack { get => m_positionWindowStack; set => Set(ref m_positionWindowStack, value); }
         public string NewProjectName { get => m_newProjectName; set => Set(ref m_newProjectName, value); }
         public string NewProjectPath { get => m_newProjectPath; set => Set(ref m_newProjectPath, value); }
         public ICommand OpenCreateProjectWindow { get; }
@@ -19,16 +26,19 @@ namespace SkyForgeEditor.ViewModel
 
         public ICommand CancelCreateProjectCommand { get; }
 
+        private ObservableCollection<ProjectTemplate>? m_projectTemplates;
         private Thickness m_positionWindowStack;
         private string m_newProjectName;
         private string m_newProjectPath;
 
         public ProjectBrowserWindowViewModel()
         {
+
             m_positionWindowStack = POSITION_WINDOW_CREATE_PROJECT;
             m_newProjectName = "NewProject";
             m_newProjectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\SkyForgeProject\";
 
+            LoadTemplateProject();
 
             OpenCreateProjectWindow = new LamdaCommand(OpenCreateProjectWindowExecuteCommand);
             OpenProjectWindow = new LamdaCommand(OpenProjectWindowExecuteCommand);
@@ -43,6 +53,24 @@ namespace SkyForgeEditor.ViewModel
         private void OpenProjectWindowExecuteCommand(object sender)
         {
             PositionWidnowStack = POSITION_WINDOW_PROJECT;
+        }
+
+
+        private void LoadTemplateProject()
+        {
+            m_projectTemplates = new ObservableCollection<ProjectTemplate>();
+            ProjectTempaltes = new ReadOnlyObservableCollection<ProjectTemplate>(m_projectTemplates);
+            var files = Directory.GetFiles(m_templatePath, "template.xml", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                var template = Serializer.FromFile<ProjectTemplate>(file);
+                template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png"));
+                template.Icon = File.ReadAllBytes(template.IconFilePath);
+                template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+                template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+                m_projectTemplates.Add(template);
+            }
         }
     }
 }
