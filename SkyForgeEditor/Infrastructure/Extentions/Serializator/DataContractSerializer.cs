d77be6c8204a332a74bcbc4ace9454b23305153e
Hard_Code <zero-0_0@bk.ru>
2024-08-26 01:23:20 +0500

    Review Serializer

--- SkyForgeEditor/Infrastructure/Extentions/Serializator/DataContractSerializer.cs
+++ SkyForgeEditor/Infrastructure/Extentions/Serializator/DataContractSerializer.cs
@@ -1,16 +1,15 @@
 ﻿using System.IO;
-using System.Runtime.Serialization;
 
-namespace SkyForgeEditor.Extentions
+namespace SkyForgeEditor.Infrastructure.Extentions.Serializator
 {
-    internal static class Serializer
+    internal class DataContractSerializer : ISerializer
     {
-        public static void ToFile<T>(T instance, string path)
+        public void SaveToFile<T>(T instance, string path)
         {
             try
             {
                 using var filesystem = new FileStream(path, FileMode.Create);
-                var serializer = new DataContractSerializer(typeof(T));
+                var serializer = new System.Runtime.Serialization.DataContractSerializer(typeof(T));
                 serializer.WriteObject(filesystem, instance);
             }
             catch
@@ -19,18 +18,18 @@ namespace SkyForgeEditor.Extentions
             }
         }
 
-        public static T? FromFile<T>(string path)
+        public T? LoadFromFile<T>(string path)
         {
             try
             {
                 using var filesystem = new FileStream(path, FileMode.Open);
-                var serializer = new DataContractSerializer(typeof(T));
+                var serializer = new System.Runtime.Serialization.DataContractSerializer(typeof(T));
                 return (T?)serializer.ReadObject(filesystem);
             }
             catch
             {
                 //TODO: get the log error
-                return default(T);
+                return default;
             }
         }
     }
---
+++ SkyForgeEditor/Infrastructure/Extentions/Serializator/ISerializer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SkyForgeEditor.Infrastructure.Extentions.Serializator
+{
+    public interface ISerializer
+    {
+        void SaveToFile<T>(T instance, string path);
+
+        T? LoadFromFile<T>(string path);
+    }
+}
